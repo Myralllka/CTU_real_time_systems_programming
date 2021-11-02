e7f1d3bebbf399fe3d8a93d5cb84acc5289a5dae
Myralllka <morgunenko@ucu.edu.ua>
2021-11-02 22:36:59 +0100

    continued lab4

--- lab4_priority_inheritance/main.c
+++ lab4_priority_inheritance/main.c
@@ -5,7 +5,7 @@
 #include <taskLib.h>
 #include <semLib.h>
 #include <sysLib.h>
-
+#include <cpuset.h>
 #include "config.h"
 
 SEM_ID global_mutex;
@@ -108,29 +108,38 @@ int main(int argc, char* argv[]) {
 //	printf("%s\n", "Measurement started");
 //	
 //	printf("%s\n", "Measurement finished");
-	int l, m, h;
+	int low, middle, high;
 
 	/* Create the task but only activate it after setting its affinity */
-	l = taskCreate("tLPrio", 210, 0, 4096, (FUNCPTR) digger_up_the_hole, i, 0, 0, 0, 0, 0, 0, 0, 0, 0);
-	l = taskCreate("tMPrio", 210, 0, 4096, (FUNCPTR) digger_up_the_hole, i, 0, 0, 0, 0, 0, 0, 0, 0, 0);
-	l = taskCreate("tHPrio", HIGH_PRIORITY, 0, 4096, (FUNCPTR) digger_up_the_hole, i, 0, 0, 0, 0, 0, 0, 0, 0, 0);
+	low = taskCreate("tLPrio", LOW_PRIORITY, 0, 4096, (FUNCPTR) tLPrio, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0);
+	middle = taskCreate("tMPrio", MID_PRIORITY, 0, 4096, (FUNCPTR) tMPrio, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0);
+	high = taskCreate("tHPrio", HIGH_PRIORITY, 0, 4096, (FUNCPTR) tHPrio, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0);
 
-	if (tid == NULL)
+	if (low == NULL || middle == NULL || high == NULL)
 	    return (ERROR);
 
 	/* Clear the affinity CPU set and set index for CPU 1 */
 	CPUSET_ZERO (affinity);
 	CPUSET_SET (affinity, 1);
 
-	if (taskCpuAffinitySet (tid, affinity) == ERROR)
-	{
+	if (taskCpuAffinitySet ( low, affinity) == ERROR) {
 	    /* Either CPUs are not enabled or we are in UP mode */
-	    taskDelete (tid);
+	    taskDelete (low);
 	    return (ERROR);
+	}
+	if (taskCpuAffinitySet(middle, affinity) == ERROR) {
+		 taskDelete(middle);
+		 return (ERROR);
+	}
+	if (taskCpuAffinitySet(high, affinity) == ERROR) {
+		taskDelete(high);
+		return (ERROR);
 	}
 
 	/* Now let the task run on CPU 1 */
-	taskActivate (tid);
+	taskActivate (low);
+	taskActivate(middle);
+	taskActivate(high);
 	
 	// Measured worst-case mutex blocking time is printed to standard output each 
 	// time it changes as integer number of milliseconds (without units) followed 
@@ -138,6 +147,5 @@ int main(int argc, char* argv[]) {
 	
 	// To achieve higher clock precision use sysClkRateSet() function.
 	
-	printf("Hello world\n");
 	return 0;
 }
